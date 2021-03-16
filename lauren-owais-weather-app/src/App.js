e50e975967be12d8dcfd1cd32500047e04997d8b
Owais <blackhawk2399@gmail.com>
2021-03-16 18:57:33 +0500

    We should be able to test our components now

--- lauren-owais-weather-app/src/App.js
+++ lauren-owais-weather-app/src/App.js
@@ -1,14 +1,13 @@
-import DailyView from "./components/DailyView"
-import WeeklyView from "./components/WeeklyView"
+import DailyView from "./components/DailyView";
+import WeeklyView from "./components/WeeklyView";
+
 function App() {
-  const WeeklyOverview = weatherData.map(item => <WeeklyView key={item.id} item={item}/>)
-
-
-  return (
-    <div>Hello World!
-    <WeeklyOverview />
-    </div>
-  );
+	return (
+		<div>
+			<DailyView></DailyView>
+			<WeeklyView></WeeklyView>
+		</div>
+	);
 }
 
 export default App;
