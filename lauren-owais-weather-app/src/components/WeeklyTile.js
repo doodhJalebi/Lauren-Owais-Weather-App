6e823680fb15c52c7ef354d907b05c3535bda385
Lauren Bichelmeir <l.bichelmeir@gmail.com>
2021-03-17 14:20:09 +0800

    added design to weekly view and tiles

--- lauren-owais-weather-app/src/components/WeeklyTile.js
+++ lauren-owais-weather-app/src/components/WeeklyTile.js
@@ -1,15 +1,18 @@
 import React from "react";
 import WeeklyView from "./WeeklyView";
 import weatherData from "../weatherData";
+import styles from "./weeklyTile.module.css"; 
 
 function WeeklyTile(props) {
     return (
-        <div className="WeeklyTile">
-            <p className="date">{props.item.text}</p>
+        <div className={styles.weeklyTileContainer}>
+            <h4 className={styles.date}>{props.item.text}</h4>
             {/* <span className="weather">{props.item.weather} </span> */}
-            <img src={props.item.weather} width="50" height="50" />
-            <span> {props.item.temphigh} </span>
-            <span className="templow"> {props.item.templow} </span>
+            <img src={props.item.weather} className={styles.weatherImage} />
+            <span className={styles.tempText}>
+                <p> {props.item.temphigh} </p>
+                <p className={styles.templow}> {props.item.templow} </p>
+            </span>
         </div>
     )
 }
--- lauren-owais-weather-app/src/components/WeeklyView.js
+++ lauren-owais-weather-app/src/components/WeeklyView.js
@@ -1,11 +1,12 @@
 import React from "react";
 import WeeklyTile from "./WeeklyTile";
 import weatherData from "../weatherData";
- 
+import styles from "./weeklyView.module.css"; 
+
 function WeeklyView() {
     const WeeklyOverview = weatherData.map(item => <WeeklyTile key={item.id} item={item}/>)
     return (
-        <div>
+        <div className={styles.weeklyViewContainer}>
             {WeeklyOverview}
         </div>
     );
